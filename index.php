b6be0e6d02ded6f8689f0b68e0148db64797d6f6
mazib <m_azib@hotmail.com>
2014-04-05 20:39:08 +0200

    cleaned up randbox & featured area css

--- index.php
+++ index.php
@@ -3,13 +3,13 @@
 	<div id="main-content">
 
 
-	<div id="randbox"><div class="attn center">
+	<div id="randbox"><div class="center"><center>
 <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
 <!-- Radcircle - index main full width ad -->
 <ins class="adsbygoogle"
      style="display:inline-block;width:970px;height:90px"
      data-ad-client="ca-pub-9545787544514574"
-     data-ad-slot="8921573154"></ins>
+     data-ad-slot="8921573154"></ins></center>
 <script>
 (adsbygoogle = window.adsbygoogle || []).push({});
 </script>
