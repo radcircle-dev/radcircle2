a096f20e1ac3cf95aa25502106b0b043ab7f6072
mazib <m_azib@hotmail.com>
2014-04-07 01:50:20 +0200

    team.php -alan +ben

---
+++ team.php
@@ -0,0 +1,121 @@
+<?php
+ 
+require_once('wp-load.php');
+ 
+//if you put in "your-wordpress-root/wp-content/" folder , prepend with "../" to go "1 folder up"
+//require_once('../wp-load.php');
+ 
+?>
+
+<?php get_header(); ?>
+
+
+
+<div id="team-box">
+
+<div id="team-intro">
+<h1>Hey. We're Radcircle.</h1>
+<p class="team-intro">Meet the guys and gals behind it all.</p>
+</div>
+
+<div id="team-profiles">
+	<ul>
+		<li><img src="images/team/mohammad.png">
+		<h1>Mohammad Azib</h1>
+		<p>Founder &amp; CEO</p>
+
+		<li><img src="images/team/filippo.png">
+		<h1>Filippo Fagnoni</h1>
+		<p>Senior Vice President</p></li>
+
+		<li><img src="images/team/aj.png">
+		<h1>AJ D'Abbracci</h1>
+		<p>Chief Technology Officer</p></li>
+
+		<li><img src="images/team/zac-bw.png">
+		<h1>Zac Hamidi</h1>
+		<p>Vice President, EDM</p></li>
+
+		<li><img src="images/team/ak.png">
+		<h1>Alex Koulos</h1>
+		<p>Vice President, Strings</p></li>
+
+		<li><img src="images/team/brandon-bw.png">
+		<h1>Brandon McKiddy</h1>
+		<p>Vice President, Flow</p></li>
+
+		<li><img src="images/team/alec-bw.png">
+		<h1>Alec Sanderson</h1>
+		<p></p></li>
+
+		<li><img src="images/team/devon-bw.png">
+		<h1>Devon Phillips</h1>
+		<p>Engineering</p></li>
+
+		<li><img src="images/team/anita-bw.png">
+		<h1>Anita Nham</h1>
+		<p>Senior Author</p></li>
+
+		<li><img src="images/team/ariel-bw.png">
+		<h1>Ariel Radow</h1>
+		<p>Author</p></li>
+
+		<li><img src="images/team/shannon-bw.png">
+		<h1>Shannon Zuber</h1>
+		<p>Author</p></li>
+
+		<li><img src="images/team/matt-bw.jpg">
+		<h1>Matt Filipek</h1>
+		<p>Editorials</p></li>
+
+		<li><img src="images/team/reid-bw.jpg">
+		<h1>Reid Flynn</h1>
+		<p>Editorials</p></li>
+
+		<li><img src="images/team/connor-bw.jpg">
+		<h1>Connor Richardson</h1>
+		<p>Operations</p></li>
+
+		<li><img src="images/team/austin-bw.jpg">
+		<h1>Austin Cisna</h1>
+		<p>Engineering</p></li>
+
+		<li><img src="images/team/chelsea-bw.jpg">
+		<h1>Chelsea Powell</h1>
+		<p>Editorials</p></li>
+
+		<li><img src="images/team/michelle-bw.jpg">
+		<h1>Michelle Andrews</h1>
+		<p>Editorials</p></li>
+
+		<li><img src="images/team/taylor-bw.jpg">
+		<h1>Taylor Glaser</h1>
+		<p>Editorials</p></li>
+
+		<li><img src="images/team/spence-bw.png">
+		<h1>Spencer Hood</h1>
+		<p>Design</p></li>
+
+		<li><img src="images/team/laura-bw.png">
+		<h1>Laura Matuszewska</h1>
+		<p>Engineering</p></li>
+
+		<li><img src="images/team/marcos-bw.jpg">
+		<h1>Marcos Assor</h1>
+		<p>Editorials</p></li>
+
+		<li><img src="images/team/ben-bw.jpg">
+		<h1>Ben Strassman</h1>
+		<p>Editorials</p></li>
+
+	</ul>
+</div>
+
+<div id="team-intro">
+<h1>Want to join?</h1>
+<p class="team-intro"><a href="http://radcircle.com/our-story">Interested in making a difference? Get involved today.</a></p>
+</div>
+
+
+</div> 
+<?php get_footer(); ?>
